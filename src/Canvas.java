c2fd47666d5ed1fce9b073c3205b535598f7adb5
aaronleechan <aaron171091@gmail.com>
2016-05-13 17:50:29 -0700

    moveSuccess

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,17 +1,99 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class Canvas extends JPanel{
 
-	private ArrayList<DShape> shapes = new ArrayList<DShape>();
+	private ArrayList<DShape> allShapes = new ArrayList<DShape>();
 	private DShape selectedShape;
 	
+	// Select the shape 
+	
+	public DShape getSelectedShape() {
+		return selectedShape;
+	}
+
+
+	public void setSelectedShape(DShape selectedShape) {
+		this.selectedShape = selectedShape;
+	}
+
+
 	public Canvas(){
 		displayCanvas();
+		
+		this.addMouseListener(new MouseAdapter(){
+			
+			public void mousePressed(MouseEvent e){
+				int x = e.getX();
+				int y = e.getY();
+				Canvas.this.selectedShape = null;
+				DShape selectedShape = locationOfShape(x,y);
+				if(selectedShape != null){
+					selectedShape.setSelected(true);
+					Canvas.this.selectedShape = selectedShape;
+					System.out.println( "arrived?");
+					
+					int indexForSelectedShape = allShapes.indexOf(selectedShape);
+					
+					for(int i = 0; i < allShapes.size(); i++)
+					{
+						if( i != indexForSelectedShape){
+							allShapes.get(i).setSelected(false);
+						}
+					}
+				}else if (selectedShape == null){
+					for(DShape s : allShapes){
+						s.setSelected(false);
+					}
+				}
+				repaint();
+			}
+			
+		});
+		
+		this.addMouseMotionListener(new MouseMotionListener(){
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				int x, y;
+				x = e.getX(); y = e.getY();
+				
+				for(DShape s : allShapes){
+					if(s.isSelected()){
+						int anchorNumber = 0;
+						if((anchorNumber = s.isAnchorChosen(x,y)) != 0){
+							
+							if(anchorNumber == 1) s.dragAnchorOne(x, y);
+							else if (anchorNumber == 2) s.dragAnchorTwo(x, y);
+							else if (anchorNumber == 3) s.dragAnchorTree(x, y);
+							else if (anchorNumber == 4) s.dragAnchorFour(x, y);
+							
+							
+						}else{
+							s.moveShapeTo(e.getX(), e.getY());
+						}
+						repaint();
+						
+					}
+				}
+				
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 	
 	
@@ -23,7 +105,7 @@ public class Canvas extends JPanel{
 	
 	public void addShape(DShape shape){
 		System.out.println("3");
-		shapes.add(shape);
+		allShapes.add(shape);
 		repaint();
 		System.out.println(" DOWN");
 	}
@@ -31,9 +113,20 @@ public class Canvas extends JPanel{
 	public void paintComponent(Graphics g){
 		System.out.println("4");
 		super.paintComponent(g);
-		for(DShape shape : shapes){
+		for(DShape shape : allShapes){
 			shape.paintComponent(g);
 		}
+	}
+	
+	public DShape locationOfShape(int x, int y){
+		DShape dshape;
+		for(int i = allShapes.size()-1; i >= 0; i--){
+			dshape = allShapes.get(i);
+			if(dshape.containsPoint(x,y)){
+				return dshape;
+			}
+		}
+		return null;
 	}
 	
 }
--- src/DShape.java
+++ src/DShape.java
@@ -3,7 +3,9 @@ import java.awt.*;
 public abstract class DShape {
 	
 	private DShapeModel dshapemodel;
-	boolean choose = false;
+	boolean choose;
+	static int ANCHORSIZE = 20;
+	public abstract Color getColor();
 	public int dimension = 30;
 	
 	
@@ -55,6 +57,15 @@ public abstract class DShape {
 	public DShapeModel getDshapemodel() {
 		return dshapemodel;
 	}
+	
+	public boolean isSelected(){
+		return this.choose;
+	}
+	
+	public void setSelected(Boolean b)
+	{
+		this.choose = b;
+	}
 
 
 
@@ -72,6 +83,135 @@ public abstract class DShape {
 
 	public void setChoose(boolean choose) {
 		this.choose = choose;
+	}
+	
+	public void moveShape(int x, int y){
+		dshapemodel.setX(x);
+		dshapemodel.setY(y);
+	}
+	
+	public void moveShapeTo(int x, int y){
+		dshapemodel.setX( x - dshapemodel.getWidth() /2);
+		dshapemodel.setY( y - dshapemodel.getHeight()/ 2);
+	}
+	
+	public int isAnchorChosen(int x, int y){
+		if(isAnchorOne(x,y)){
+			return 1;
+		}else if(isAnchorTwo(x,y)){
+			return 2;
+		}else if(isAnchorTree(x,y)){
+			return 3;
+		}else if(isAnchorFour(x,y)){
+			return 4;
+		}
+		return 0;
+	}
+	
+	public boolean isAnchorOne(int x, int y){
+		int xAxis = dshapemodel.getX();
+		int yAxis = dshapemodel.getY();
+		if( x >= xAxis - ANCHORSIZE && x <= xAxis + ANCHORSIZE && y >= yAxis - ANCHORSIZE && y <= yAxis + ANCHORSIZE)
+			return true;
+		else
+			return false;
+	}
+	public boolean isAnchorTwo(int x, int y){
+		int xAxis = dshapemodel.getX();
+		int yAxis = dshapemodel.getY();
+		if( x >= xAxis - ANCHORSIZE && x <= xAxis + ANCHORSIZE && y >= yAxis - ANCHORSIZE && y <= yAxis + ANCHORSIZE)
+			return true;
+		else
+			return false;
+	}
+	public boolean isAnchorTree(int x, int y){
+		int xAxis = dshapemodel.getX();
+		int yAxis = dshapemodel.getY();
+		if( x >= xAxis - ANCHORSIZE && x <= xAxis + ANCHORSIZE && y >= yAxis - ANCHORSIZE && y <= yAxis + ANCHORSIZE)
+			return true;
+		else
+			return false;
+	}
+	public boolean isAnchorFour(int x, int y){
+		int xAxis = dshapemodel.getX();
+		int yAxis = dshapemodel.getY();
+		if( x >= xAxis - ANCHORSIZE && x <= xAxis + ANCHORSIZE && y >= yAxis - ANCHORSIZE && y <= yAxis + ANCHORSIZE)
+			return true;
+		else
+			return false;
+	}
+
+
+
+	public boolean containsPoint(int x, int y) {
+		DShapeModel model = this.dshapemodel;
+		if(model != null){
+			int h = model.getHeight();
+			int w = model.getWidth();
+			if(x>=model.getX() && x <= model.getX() + w && y>= model.getY() && y <= model.getY() + h){
+				this.setSelected(true);
+				return true;
+			}
+			this.setSelected(false);
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+	private void setSelected(boolean b) {
+		this.choose= b;
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void dragAnchorOne(int x, int y){
+		DShapeModel shapemodel = this.dshapemodel;
+		int preX, preY,diffX, diffY;
+		preX = shapemodel.getX();
+		preY = shapemodel.getY();
+		diffX = (preX - x);
+		diffY = (preY - y);
+		moveShape(x,y);
+		dshapemodel.setWidth(dshapemodel.getWidth() + diffX);
+		dshapemodel.setHeight(dshapemodel.getHeight() + diffY);
+	}
+	public void dragAnchorTwo(int x, int y){
+		DShapeModel shapemodel = this.dshapemodel;
+		int preX, preY,diffX, diffY, originX;
+		originX = shapemodel.getX();
+		preX = shapemodel.getX() + shapemodel.getWidth();
+		preY = shapemodel.getY();
+		diffX = (x - preX);
+		diffY = (preY - y);
+		moveShape(originX,preY - diffY);
+		dshapemodel.setWidth(dshapemodel.getWidth() + diffX);
+		dshapemodel.setHeight(dshapemodel.getHeight() + diffY);
+	}
+	public void dragAnchorTree(int x, int y){
+		DShapeModel shapemodel = this.dshapemodel;
+		int preX, preY,diffX, diffY, originY, originX;
+		originX = shapemodel.getX();
+		originY = shapemodel.getY();
+		preX = shapemodel.getX();
+		preY = originY + shapemodel.getHeight();
+		diffX = (preX - x);
+		diffY = (y - preY);
+		moveShape(originX - diffX, originY);
+		dshapemodel.setWidth(shapemodel.getWidth() + diffX);
+		dshapemodel.setHeight(shapemodel.getHeight() + diffY);
+	}
+	public void dragAnchorFour(int x, int y){
+		DShapeModel shapemodel = this.dshapemodel;
+		int preX, preY,diffX, diffY,originY, originX;
+		originX = shapemodel.getX();
+		originY = shapemodel.getY();
+		
+		preX = originX + shapemodel.getWidth();
+		preY = originY + shapemodel.getHeight();
+		diffX = x - preX;
+		diffY = y - preY;
+		moveShape(x,y);
+		dshapemodel.setWidth(dshapemodel.getWidth() + diffX);
+		dshapemodel.setHeight(dshapemodel.getHeight() + diffY);
 	}
 	
 
